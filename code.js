02dca878ec1048be5fa775b75b069992e8113db0
Nicolae1188 <nicolaefrunza88@gmail.com>
2019-11-14 16:24:01 +0000

    commiting file with practice problems

---
+++ code.js
@@ -0,0 +1,397 @@
+// let favouriteDrink = "coffee";
+// console.log(favouriteDrink);
+// console.log("My favourite drink is " + favouriteDrink)
+// console.log(`My favourite drink is ${favouriteDrink}`)
+// favouriteDrink = "tea"
+// console.log(`My favourite drink is ${favouriteDrink}`)
+// let myNameIs = "Jenny"; let myAgeIs = "46"; let myFavouriteColourIs = "blue";
+// console.log(`My name is ${myNameIs} My age is ${myAgeIs} My favourite colour is ${myFavouriteColourIs}`)
+// let myBreakfast = "cereal"; let myLunch = "pasta"; let myDinner = "fish and chips";
+// console.log(`My breakfast ${myBreakfast} My lunch ${myLunch} My dinner ${myDinner}`)
+
+// myBreakfast = "toast"; 
+// myLunch = "sandwich"; 
+// myDinner = "Pizza";
+// console.log(`My breakfast ${myBreakfast} My lunch ${myLunch} My dinner ${myDinner}`)
+
+// let myBirthDate = "03/07/2020"
+// let dateToday = "11/11/2019"
+// let numberDaysToBirthdate = myBirthDate - dateToday
+// console.log(numberDaysToBirthdate)
+
+// let currentDate = new Date('November 11, 2019') / 1000
+// let futureDate = new Date('July 3, 2020') / 1000
+// let diff = futureDate - currentDate
+// console.log(diff / 60 / 60 / 24)
+// let space1 = " x " 
+// let space2 = " o "
+// let space3 = "   " 
+// let space4 = " x "    
+// let space5 = " x "
+// let space6 = "   "  
+// let space7 = " o "
+// let space8 = "   " 
+// let space9 = "   " 
+// console.log("   |   |   ")
+// console.log(`${space1}|${space2}|${space3}`)
+// console.log("   |   |   ")
+// console.log("-----------")
+// console.log("   |   |   ")
+// console.log(`${space4}|${space5}|${space6}`)
+// console.log("   |   |   ")
+// console.log("-----------")
+// console.log("   |   |   ")
+// console.log(`${space7}|${space8}|${space9}`)
+// console.log("   |   |   ")
+// if (1 === "1"){
+// console.log(true);
+// }
+// else {
+//     console.log (false)};
+
+// let age = 21
+// if (age>17) { 
+//     console.log ("serve alcohol")
+// }
+// else  {
+//     console.log ("I cannot serve you")
+// }
+
+// age = "21"
+// let country = "France"
+// if(age>17 && country == "UK") {
+//     console.log ("I can serve you alcohol")
+// }
+// else {console.log ("I cannot serve you alcohol")}
+
+
+
+// let password = "codenation"
+
+// if (password.length >= 8 ) {
+//  console.log ("password")   
+// }
+// else {console.log ("password too short")}
+
+
+
+// let number = "72"
+// if (number % 3 == 0 || number % 5 == 0 ) {
+//     console.log ("This number is divisible by 3 or 5")
+// }
+// else { console.log ("This number is not divisible by 3 or 5")}
+
+
+// number = "15"
+// if (number % 3 == 0) {
+//     console.log ("fizz")
+// }
+// else if (number % 5 == 0){
+//     console.log ("buzz")}
+// else if (number % 3 == 0 && number % 5 == 0){
+//     console.log ("fizz buzz")}
+// else {console.log ("number")}
+
+// let time = 11
+// let home = "My House" 
+// let placeOfWork = "Codenation"
+// let townOfHome = "Chester"
+// if (time <= 8){
+//     console.log ("home")}
+// else if (time >= 9 ){
+//     console.log ("placeOfWork")}
+//  else if (time > 8 || time < 9){
+//      console.log ("townOfHome")
+//  }
+
+
+//  let word = "remember"
+//  if (word.charAt(0) == word.length - 1){ 
+//      console.log ("true")
+//     }
+// else 
+// {console.log ("false")
+// }
+
+
+// let num1 = 5
+// let num2 = 8
+// let num3 = "num1 * num2"
+// if ((num1 + num2) % 2 == 0){
+//     console.log ("num1 + num2")
+// }
+// else {console.log ("num3")}
+
+
+// const coffee = ( size, type) => {
+//     console.log (size, type)
+// }
+// coffee ('large', 'americano')
+// coffee ('small', 'cappaccino')
+
+// const square = (number) => {
+//     return number * number
+// }
+// square(5)
+    
+
+// let orderCount =0
+// const takeOrder = (topping1, topping2) => {
+//     console.log(`Pizza with ${topping1}`);
+//     orderCount++;
+// }
+// takeOrder("pineapple","cheese")
+
+
+
+
+// let balance = 500
+// let pin = 1234
+
+// const checkPin = (pin) => {
+//    if (pin = 1234)
+// console.log ('correct pin')
+// else console.log ('incorrect pin')
+
+// }
+
+// const checkFunds = (amount) => {
+//     console.log('checkfunds was also called')
+//     return balance - (amount) 
+// }
+// const dispenseCash = (amount, accnum) => {
+
+// }
+
+
+
+
+
+
+
+
+
+// // {
+// // console.log(`amount, accnum`)
+// // }
+// // cashWithdrawal(' 200' , '50449921')
+
+// const multiply = (num1, num2) => {
+//     return num1 * num2
+// }
+// console.log(multiply (2,3))
+
+
+
+// let myFavouriteSongs = [ "Echo and the Bunneymen" ,"Stone roses", " Oasis"]
+
+// console.log (myFavouriteSongs[2])
+
+// myFavouriteSongs[2] = "New Band"
+// console.log (myFavouriteSongs)
+// console.log(myFavouriteSongs.length)
+// myFavouriteSongs.push ("Oasis")
+// console.log (myFavouriteSongs)
+
+// myFavouriteSongs.pop(3)
+//     console.log(3)
+
+
+
+//     let faveWeb = ["web1", "web2","web3",]
+//     faveWeb.push ("web4","web5")
+//     console.log (faveWeb)
+// //     faveWeb.pop(4)
+// //     console.log (faveWeb)
+
+// //     let colour = ["red", "blue","green" ]
+// //     colour.shift("blue")
+// //     console.log (colour)
+
+// //     colour.unshift("purple")
+// //     console.log(colour)
+
+   
+   
+
+
+
+
+// //    const multi = (num1, num2) => {
+// //        return num1 * num2
+// //    }
+// //    console.log(multi(3, 5))
+
+// // let films = [ "film1", "film2", "film3"]
+// // console.log (films)
+
+// // console.log (films [2])
+
+// // films[1]= "film4"
+// // console.log (films)
+
+// // console.log (films.length)
+
+// // films.push ("film5")
+// // console.log (films)
+
+// // films.pop (3)
+// // console.log (films)
+
+// // films.shift (0)
+// // console.log (films)
+
+// // films.unshift ("film2")
+// // console.log (films)
+
+// // films.splice(1)
+// // console.log (films)
+
+// // films.slice(0)
+// // console.log (films)
+
+
+// // let myArray = ["Elishka", "Dan", "Ashley","Connor","Ben"]
+// // console.log (myArray)
+// // myArray.pop(4)
+// // myArray.pop(3)
+// // console.log (myArray)
+
+
+
+
+
+
+// // const subSandwich = (topping1, topping2, topping3, topping4, topping5) => {
+
+// //    console.log (`subSandwich with ${topping1} ${topping2} ${topping3} ${topping4} ${topping5}`)
+
+// // }
+
+// // subSandwich ("cheese", "ham", "pepperoni", "tomatoes","chicken")
+
+
+
+
+// // /
+// // let cards = [ "diamond", "spade","heart", "club"];
+// // let currentCard = "diamond";
+
+// // while(currentCard != "spade"){
+// //     console.log(currentCard);
+// //     currentCard = cards [Math.floor(Math.random()*4)];
+// // }
+
+// // console.log(currentCard);
+
+
+
+
+// let film = ["avengers", "toy story", "ghostbusters", "spiderman" ];
+
+// const filmCheck = () => {
+
+// for (let i = 0; i < film.length; i++){
+
+// if(film[i] == "ghostbusters"){
+// console.log ("yey it's ghostbusters")
+// } else {
+//     console.log ("booo,we want ghostbusters")
+// }
+// }
+// }
+// filmCheck()
+
+
+
+
+
+// let currentFish = 20
+
+// while (currentFish > 10) {
+//     console.log ("too many fishes")
+//     currentFish = currentFish - 1
+// }
+
+// let myArray = ["Elishka", "Dan", " Ashley", "Connor", "Ben"]
+
+// console.log(myArray.sort())
+
+
+// let myString = "emily"
+// let myArray = myString.split('');
+// console.log (myArray)
+
+
+
+
+// const person = {
+// name: "Jenny",
+// age: 46,
+//   sayHi(){ 
+//       console.log(`hello, my name is ${this.name}`)
+
+//   }
+// }
+
+// person.songs = [ "song1","song2","song3"]
+
+// person.sayHi()
+
+// // console.log (person)
+
+// console.log(person.name)
+
+// console.log(person["name"])
+
+
+
+
+
+// let day = " Monday "
+
+// const alarm = {
+
+//     weekendAlarm: "no alarm needed",
+//     weekdayAlarm: "get up at 7am "
+
+// }
+
+// if (day == "Saturday" || day=="Sunday" ){ 
+//     console.log (alarm.weekendAlarm)
+// }else {
+//     console.log (alarm.weekdayAlarm)
+// }
+
+
+// const pet = {
+//     name: "Trixie",
+//     typeOfPet: "Dog" ,
+//     Age: 9,
+//     Colour: "White",
+
+//      eat () {
+//          console.log (`${this.name} is eating`)},
+
+//      drink () {
+//          console.log(`${this.name} is eating`)
+//      }
+    
+// }
+
+// pet.drink()
+
+// const array1 = ["1,2,3,4,5"]
+
+// const firstElement = array1.shift()
+// // console.log(array1)
+// console.log(firstElement)
+
+// arr = [1,2,3,4,5]
+
+// let firstValue = arr[0]
+
+// console.log (firstValue)
+
+
+
